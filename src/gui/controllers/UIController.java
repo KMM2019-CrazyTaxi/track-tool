8b8d3cefcb7fe70bf143addf38b9ea5c153c7a28
Henrik Nilsson <henni142@student.liu.se>
2019-12-06 18:53:13 +0100

    Add buttons to ToolBar in UIController initialize method. Add tool button styling in default.css.

--- src/gui/controllers/UIController.java
+++ src/gui/controllers/UIController.java
@@ -1,7 +1,11 @@
 package gui.controllers;
 
+import editor.Editor;
+import editor.Tools;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 
@@ -15,5 +19,23 @@ public class UIController {
         // Make Map View fit all available space
         mapViewFeature.prefWidthProperty().bind(uiScene.widthProperty().subtract(toolBar.widthProperty()));
         mapViewFeature.prefHeightProperty().bind(uiScene.heightProperty());
+
+        // Make scroll pane wide enough
+
+
+        // Add tools
+        for (Tools t : Tools.values()) {
+            Button b = new Button();
+            b.addEventHandler(MouseEvent.MOUSE_CLICKED, UIController::handleToolBarButtonClick);
+            b.setGraphic(t.getIcon());
+            b.setId(t.name());
+            b.getStyleClass().add("toolButton");
+            toolBar.getChildren().add(b);
+        }
+    }
+
+    private static void handleToolBarButtonClick(MouseEvent e) {
+        Tools tool = Tools.valueOf(((Button)e.getSource()).getId());
+        Editor.getInstance().activeTool.update(tool);
     }
 }
